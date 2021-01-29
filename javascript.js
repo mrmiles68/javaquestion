c10972f15b182b309b197f6d3323f82b232d4ba5
mrmiles68 <cheaptechsupport2000@gmail.com>
2021-01-29 10:00:08 -0800

    copied from bootcamp prework

---
+++ javascript.js
@@ -0,0 +1,32 @@
+<script>
+
+<script type="text/javascript">
+
+        document.getElementById("button1").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "250px";
+
+        });
+
+        document.getElementById("button2").addEventListener("click", function(){
+
+            document.getElementById("box").background-color: blue
+
+        });
+
+        document.getElementById("button3").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "250px";
+
+});
+
+        document.getElementById("button4").addEventListener("click", function(){
+
+            document.getElementById("box").style="height:150px; width:150px; background-color: orange; margin:25px";
+
+});
+
+
+
+
+</script>
