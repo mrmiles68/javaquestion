c27f050afd622cfd891937fd93334e70887d5813
mrmiles68 <cheaptechsupport2000@gmail.com>
2021-01-29 11:00:25 -0800

    upgraded JavaScript with corrected syntax

--- javascript.js
+++ javascript.js
@@ -1,32 +1,30 @@
-<script>
 
-<script type="text/javascript">
 
-        document.getElementById("button1").addEventListener("click", function(){
+document.getElementById("button1").addEventListener("click", function () {
 
-            document.getElementById("box").style.height = "250px";
-
-        });
-
-        document.getElementById("button2").addEventListener("click", function(){
-
-            document.getElementById("box").background-color: blue
-
-        });
-
-        document.getElementById("button3").addEventListener("click", function(){
-
-            document.getElementById("box").style.height = "250px";
+    document.getElementById("box").style.height = "250px";
 
 });
 
-        document.getElementById("button4").addEventListener("click", function(){
+document.getElementById("button2").addEventListener("click", function () {
 
-            document.getElementById("box").style="height:150px; width:150px; background-color: orange; margin:25px";
+    // document.getElementById("box").background-color: blue;
+    document.getElementById("box").style.backgroundColor = "blue";
+    // document.getElementById("box").style = "background-color: blue";
+});
+
+document.getElementById("button3").addEventListener("click", function () {
+
+    document.getElementById("box").style.height = "250px";
+
+});
+
+document.getElementById("button4").addEventListener("click", function () {
+
+    document.getElementById("box").style = "height:150px; width:150px; background-color: orange; margin:25px";
 
 });
 
 
 
 
-</script>
